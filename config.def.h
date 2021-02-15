3729e8f6d0d40f91238fa362cdf0e5cedf409f0f
Soura99 <souradeep4321@outlook.com>
2021-02-15 13:39:21 +0530

    patch applied: focusmaster

--- config.def.h
+++ config.def.h
@@ -93,7 +93,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }, 
+	{ MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },
 };
 
 /* button definitions */
